bd8e0705760086a34d7f34d58558dd7a07c68d6d
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-01-01 18:52:44 -0300

    ajustando adicao de tarefas e mensagens

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 const containerTarefa = document.getElementById('tarefas')
-
+const mensagem = document.getElementById('mensagem')
 function addTarefa(){
-    containerTarefa.innerHTML += ` <div class="tarefa"><p>Fazer compras</p></div>
+    
+    const inputTarefa = document.getElementById('inputTarefa').value
+    if(!inputTarefa) {
+        mensagem.innerHTML = `<p class="mensagemN">Escreva uma tarefa antes!</p>`
+    }
+    else {
+        mensagem.innerHTML = `<p class="mensagemP">Tarefa adicionada com sucesso!</p>`
+        containerTarefa.innerHTML += ` <div class="tarefa">
+                                        <p>${inputTarefa}</p>
+                                        <div class="botoesTarefa">
+                                            <button class="botaoTarefa">Excluir</button>
+                                        </div>
+                                    </div>
     `
+    }
+    
 }
