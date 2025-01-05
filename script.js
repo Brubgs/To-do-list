ca1e58cc839a930f1ea06b6b54a139bce2c5a5f3
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-01-05 12:32:42 -0300

    ajustando excluir tarefa

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
 const containerTarefa = document.getElementById('tarefas')
 const mensagem = document.getElementById('mensagem')
+
 function addTarefa(){
     
-    const inputTarefa = document.getElementById('inputTarefa').value
-    if(!inputTarefa) {
+    const inputTarefa = document.getElementById('inputTarefa')
+    const tarefa = inputTarefa.value
+    if(!tarefa) {
         mensagem.innerHTML = `<p class="mensagemN">Escreva uma tarefa antes!</p>`
     }
     else {
         mensagem.innerHTML = `<p class="mensagemP">Tarefa adicionada com sucesso!</p>`
-        containerTarefa.innerHTML += ` <div class="tarefa">
-                                        <p>${inputTarefa}</p>
-                                        <div class="botoesTarefa">
-                                            <button class="botaoTarefa">Excluir</button>
-                                        </div>
-                                    </div>
-    `
-    }
+
+        const novaTarefa = document.createElement('div')
+        novaTarefa.className = 'tarefa'
+        novaTarefa.innerHTML = `<p>${tarefa}</p>
+                                <div class="botoesTarefa">
+                                    <button class="botaoTarefa" id="botaoExcluir">Excluir</button>
+                                </div>`
+        containerTarefa.appendChild(novaTarefa)
+
+        const botaoExcluir = novaTarefa.querySelector('.botaoTarefa')
+        botaoExcluir.addEventListener('click', () => {
+            //alert('Excluir tarefa')
+            novaTarefa.remove()
+            mensagem.innerHTML = `<p class="mensagemN">Tarefa excluída!</p>`
+        })
+
     
+    }
+    inputTarefa.value = ''
 }
